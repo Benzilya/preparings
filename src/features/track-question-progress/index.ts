674f99c30593100274ebcab584cff59d66e0ea31
Ilia <benzilya@yandex.ru>
2026-08-06 18:58:49 +0700

    feat(progress): export favorites page

--- src/features/track-question-progress/index.ts
+++ src/features/track-question-progress/index.ts
@@ -6,6 +6,7 @@ export {
   summarizeProgress,
 } from "./model/storage";
 export { useQuestionProgress } from "./model/use-question-progress";
+export { FavoriteQuestionsPage } from "./ui/favorite-questions-page";
 export { ProgressDashboard } from "./ui/progress-dashboard";
 export { ProgressPage } from "./ui/progress-page";
 export { QuestionProgressControls } from "./ui/question-progress-controls";
