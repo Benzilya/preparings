db2449189b00e56cc843e67f3ebaeca71356326b
Ilia <benzilya@yandex.ru>
2026-08-06 20:03:14 +0700

    Expose settings restoration API

--- src/features/manage-settings/index.ts
+++ src/features/manage-settings/index.ts
@@ -1,3 +1,12 @@
-export { defaultSettings, readSettings, resetSettings, writeSettings } from "./model/settings";
+export {
+  applySettings,
+  defaultSettings,
+  readSettings,
+  resetSettings,
+  restoreSettings,
+  settingsChangedEvent,
+  writeSettings,
+} from "./model/settings";
 export type { CatalogDensity, InterfaceLanguage, UserSettings } from "./model/settings";
+export { useSettings } from "./model/use-settings";
 export { SettingsPage } from "./ui/settings-page";
