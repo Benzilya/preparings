75c310f88a8c9ce67a0871edc125e66c96b0fcc3
Ilia <benzilya@yandex.ru>
2026-08-07 04:18:12 +0700

    feat: export question content translations

--- src/features/manage-settings/index.ts
+++ src/features/manage-settings/index.ts
@@ -8,6 +8,7 @@ export {
   writeSettings,
 } from "./model/settings";
 export type { CatalogDensity, InterfaceLanguage, UserSettings } from "./model/settings";
+export { getQuestionContentTranslations } from "./model/question-content-translations";
 export { getTranslations } from "./model/translations";
 export type { InterfaceTranslations } from "./model/translations";
 export { useSettings } from "./model/use-settings";
