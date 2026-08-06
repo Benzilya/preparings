5321c18378880609d5175f2c61446c825ec26091
Ilia <benzilya@yandex.ru>
2026-08-06 19:51:45 +0700

    Export settings feature API

---
+++ src/features/manage-settings/index.ts
@@ -0,0 +1,3 @@
+export { defaultSettings, readSettings, resetSettings, writeSettings } from "./model/settings";
+export type { CatalogDensity, InterfaceLanguage, UserSettings } from "./model/settings";
+export { SettingsPage } from "./ui/settings-page";
