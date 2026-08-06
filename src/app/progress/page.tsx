ba658706bc01713cc1cfd14151ec73b80c90970e
Ilia <benzilya@yandex.ru>
2026-08-07 04:54:54 +0700

    feat: use top 100 questions in progress

--- src/app/progress/page.tsx
+++ src/app/progress/page.tsx
@@ -1,6 +1,6 @@
-import { seedQuestions } from "@/../content/questions/seed";
+import { top100Questions } from "@/../content/questions/top100";
 import { ProgressPage } from "@/features/track-question-progress";
 
 export default function ProgressRoute() {
-  return <ProgressPage questions={seedQuestions} />;
+  return <ProgressPage questions={top100Questions} />;
 }
